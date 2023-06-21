8797d5dc2cfbc55c3f62cbe0ce7b3a287a11e948
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-06-21 02:09:45 -0500

    💄 Add the navbar

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,8 +1,14 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
+      colors: {
+        "primary": colors.amber["500"],
+        "secondary": "#56CCF2",
+      },
       fontFamily: {
         quicksand: ["Quicksand", "sans-serif"],
       },
