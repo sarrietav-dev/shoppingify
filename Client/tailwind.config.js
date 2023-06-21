c2037f81ca9b7d5da93b6e0a17153cee8b27b3f1
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-06-20 23:16:21 -0500

    🎨 Format project with prettier

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       fontFamily: {
-        quicksand: ['Quicksand', 'sans-serif'],
-      }
+        quicksand: ["Quicksand", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
