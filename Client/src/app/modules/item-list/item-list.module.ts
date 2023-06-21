e54d3428a7fc7f23f9a2218b9b8a941933a9ea5e
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-06-21 03:05:12 -0500

    🚧 Export ItemDashboard

--- Client/src/app/modules/item-list/item-list.module.ts
+++ Client/src/app/modules/item-list/item-list.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemDashboardComponent } from './pages/item-dashboard/item-dashboard.component';
 
-
-
 @NgModule({
-  declarations: [
-    ItemDashboardComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  exports: [ItemDashboardComponent],
+  declarations: [ItemDashboardComponent],
+  imports: [CommonModule],
 })
-export class ItemListModule { }
+export class ItemListModule {}
