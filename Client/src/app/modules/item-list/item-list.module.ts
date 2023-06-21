1433b3a09b465226551cce8350146b90698b97f3
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-06-21 03:04:31 -0500

    🚧 Create the item list module
    WIth its page component

---
+++ Client/src/app/modules/item-list/item-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ItemDashboardComponent } from './pages/item-dashboard/item-dashboard.component';
+
+
+
+@NgModule({
+  declarations: [
+    ItemDashboardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ItemListModule { }
---
+++ Client/src/app/modules/item-list/pages/item-dashboard/item-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'item-dashboard',
+  templateUrl: './item-dashboard.component.html',
+  styleUrls: ['./item-dashboard.component.css']
+})
+export class ItemDashboardComponent {
+
+}
