dee079b1e25271db750bfbcde77ac5f2c87bd53e
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-06-21 02:43:50 -0500

    💄 Add tooltips to the navbar links

--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
-
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  exports: [
-    NavbarComponent
-  ],
-  declarations: [
-    NavbarComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  exports: [NavbarComponent],
+  declarations: [NavbarComponent],
+  imports: [CommonModule, MatTooltipModule],
 })
-export class CoreModule { }
+export class CoreModule {}
