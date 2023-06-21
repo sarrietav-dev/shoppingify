e63e1edb953f45e91acac1dac53ad6aa9a31e78f
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-06-21 02:15:56 -0500

    ♻️ Move the navbar to the core module

---
+++ Client/src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+
+
+@NgModule({
+  exports: [
+    NavbarComponent
+  ],
+  declarations: [
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
