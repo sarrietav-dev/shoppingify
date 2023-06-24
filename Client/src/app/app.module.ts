925c874370b83af1bf41f4744fec714723b09d89
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-06-24 11:00:18 -0500

    💄 Finish the item dashboard

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemListModule } from './modules/item-list/item-list.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     CoreModule,
     BrowserAnimationsModule,
+    ItemListModule
   ],
   providers: [],
   bootstrap: [AppComponent],
