f0c4ddd42bdeeccd9c23b3a806443d7f644750eb
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-06-24 11:15:47 -0500

    🚧 Create the shopping cart component

--- Client/src/app/modules/shopping-cart/shopping-cart.module.ts
+++ Client/src/app/modules/shopping-cart/shopping-cart.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  exports: [ShoppingCartComponent],
+  declarations: [ShoppingCartComponent],
+  imports: [CommonModule],
 })
-export class ShoppingCartModule { }
+export class ShoppingCartModule {}
