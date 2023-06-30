886d6057c2be6e9aa2106d78cd0c69b55e5c8f58
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-06-30 11:11:31 -0500

    🚧 Create input component

--- Client/src/app/modules/create-item-form/create-item-form.module.ts
+++ Client/src/app/modules/create-item-form/create-item-form.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateItemFormComponent } from './pages/create-item-form/create-item-form.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
-  declarations: [CreateItemFormComponent],
+  declarations: [CreateItemFormComponent, InputComponent],
   imports: [CommonModule],
   exports: [CreateItemFormComponent],
 })
