0dc6f93ee6cfcdec8df9aedb19570b7b2130cf55
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-07-03 13:24:09 -0500

    💄 Create the create-item-form (#4)
    * 🚧 Add the create-item-form module
    
    * 🚧 Create input component
    
    * 💄 Style the input component
    
    * 💄 Finish styling the form
    
    The input component was deleted for easiness when developing reactive forms
    
    * 🚧 Use ReactiveForms
    
    * ✨ Add validation to the form

---
+++ Client/src/app/modules/create-item-form/pages/create-item-form/create-item-form.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-item-form',
+  templateUrl: './create-item-form.component.html',
+  styleUrls: ['./create-item-form.component.css'],
+})
+export class CreateItemFormComponent {
+  constructor(private fb: FormBuilder) {}
+
+  createItemForm = this.fb.group({
+    name: ['', Validators.required],
+    note: [''],
+    image: [
+      '',
+      Validators.pattern('(http(s?):)([/|.|\\w|\\s|-])*\\.(?:jpg|gif|png)'),
+    ],
+    category: ['', Validators.required],
+  });
+
+  onSubmit() {
+    console.log(this.createItemForm.value);
+  }
+}
