e121bd05abe5fba26c64412e03494da04a8572ec
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-07-03 10:52:33 -0500

    🚧 Use ReactiveForms

--- Client/src/app/modules/create-item-form/create-item-form.module.ts
+++ Client/src/app/modules/create-item-form/create-item-form.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateItemFormComponent } from './pages/create-item-form/create-item-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CreateItemFormComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [CreateItemFormComponent],
 })
 export class CreateItemFormModule {}
--- Client/src/app/modules/create-item-form/pages/create-item-form/create-item-form.component.ts
+++ Client/src/app/modules/create-item-form/pages/create-item-form/create-item-form.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create-item-form',
   templateUrl: './create-item-form.component.html',
-  styleUrls: ['./create-item-form.component.css']
+  styleUrls: ['./create-item-form.component.css'],
 })
 export class CreateItemFormComponent {
+  constructor(private fb: FormBuilder) {}
 
+  createItemForm = this.fb.group({
+    name: [''],
+    note: [''],
+    image: [''],
+    category: [''],
+  });
+
+  onSubmit() {
+    console.log(this.createItemForm.value);
+  }
 }
