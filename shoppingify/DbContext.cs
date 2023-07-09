d5bd0c7e0f0eb2dd86960da4351a702c5b6ff78c
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-07-09 14:16:47 -0500

    Create shopping db context

---
+++ shoppingify/DbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace shoppingify;
+
+internal class ShoppingContext : DbContext
+{
+    public ShoppingContext(DbContextOptions<ShoppingContext> options) : base(options)
+    {
+    }
+}
