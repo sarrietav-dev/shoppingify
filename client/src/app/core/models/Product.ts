7925d913165b4c82b2ea7737c3b2455555801651
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-07-21 12:20:21 -0500

    🎨 Format code

--- client/src/app/core/models/Product.ts
+++ client/src/app/core/models/Product.ts
@@ -1,7 +1,7 @@
 export type Product = {
-    id: string;
-    name: string;
-    category: string;
-    note?: string;
-    link?: string;
-}
+  id: string;
+  name: string;
+  category: string;
+  note?: string;
+  link?: string;
+};
