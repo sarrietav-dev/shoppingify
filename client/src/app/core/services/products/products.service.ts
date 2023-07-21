1a909d671223a3940b883f1acdff748aee6dfc21
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-07-21 12:14:53 -0500

    Create the product service

--- client/src/app/core/services/products/products.service.ts
+++ client/src/app/core/services/products/products.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Product } from '../../models/Product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  private baseUrl = environment.baseUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProducts() {
+    return this.http.get<Product[]>(`${this.baseUrl}/api/products`);
+  }
+
+  getProduct(id: string) {
+    return this.http.get<Product>(`${this.baseUrl}/api/products/${id}`);
+  }
+
+  createProduct(product: Product) {
+    return this.http.post<Product>(`${this.baseUrl}/api/products`, product);
+  }
+
+  getCategories() {
+    return this.http.get<string[]>(`${this.baseUrl}/api/products/categories`);
+  }
 }
