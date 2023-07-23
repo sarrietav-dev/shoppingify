2f816e64d9d331a98c2f5cc13dd4fd2b9bb7a91b
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-07-23 17:54:29 -0500

    ✨ Show products

--- client/src/app/modules/item-list/pages/item-dashboard/item-dashboard.component.ts
+++ client/src/app/modules/item-list/pages/item-dashboard/item-dashboard.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/core/models/Product';
+import { ProductsService } from 'src/app/core/services/products/products.service';
 
 @Component({
   selector: 'item-dashboard',
   templateUrl: './item-dashboard.component.html',
   styleUrls: ['./item-dashboard.component.css'],
 })
-export class ItemDashboardComponent {}
+export class ItemDashboardComponent implements OnInit {
+  products: ProductsGroupedByCategory[] = []
+
+  constructor(private productService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((products) => {
+      this.products = this.groupProductsByCategory(products);
+    });
+  }
+  groupProductsByCategory(products: Product[]): ProductsGroupedByCategory[] {
+    const groupedProducts = products.reduce((groupedProducts, product) => {
+      const category = product.category;
+      if (groupedProducts[category] == null) {
+        groupedProducts[category] = [];
+      }
+      groupedProducts[category].push(product);
+      return groupedProducts;
+    }, {} as { [key: string]: Product[] });
+
+    return Object.keys(groupedProducts).map((category) => {
+      return {
+        category,
+        products: groupedProducts[category],
+      };
+    });
+  }
+}
+
+type ProductsGroupedByCategory = {
+  category: string;
+  products: Product[];
+}
