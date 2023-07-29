0aeb4378110ff8c5ae61e9c28498986b20318eb0
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-07-28 22:14:59 -0500

    💄 Make the category autocomplete work

--- client/src/app/modules/shopping-cart/components/create-item-form/create-item-form.component.ts
+++ client/src/app/modules/shopping-cart/components/create-item-form/create-item-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { filter } from 'rxjs';
 import { ProductsService } from 'src/app/core/services/products/products.service';
 
 @Component({
@@ -7,21 +8,36 @@ import { ProductsService } from 'src/app/core/services/products/products.service
   templateUrl: './create-item-form.component.html',
   styleUrls: ['./create-item-form.component.css'],
 })
-export class CreateItemFormComponent {
+export class CreateItemFormComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private productService: ProductsService
   ) {}
 
   @Output() cancelForm = new EventEmitter<ShoppingCartState>();
+  categories: string[] = [];
+  matchingCategories: string[] = [];
+
+  ngOnInit(): void {
+    this.productService.getCategories().subscribe((categories) => {
+      this.categories = categories;
+    });
+
+    this.createItemForm
+      .get('category')
+      ?.valueChanges.subscribe((value) => {
+        if (value !== null) {
+          this.matchingCategories = this.categories.filter((category) =>
+            category.toLowerCase().includes(value.toLowerCase())
+          );
+        }
+      });
+  }
 
   createItemForm = this.fb.group({
     name: ['', Validators.required],
     note: [''],
-    image: [
-      '',
-      Validators.pattern('(http(s?):)([/|.|\\w|\\s|-])*'),
-    ],
+    image: ['', Validators.pattern('(http(s?):)([/|.|\\w|\\s|-])*')],
     category: ['', Validators.required],
   });
 
@@ -44,4 +60,14 @@ export class CreateItemFormComponent {
   onCancel() {
     this.cancelForm.emit('editing');
   }
+
+  get shouldShowAutocomplete() {
+    return this.createItemForm.get('category')?.value !== '' && this.matchingCategories.length > 0;
+  }
+
+  setCategory(category: string) {
+    this.createItemForm.get('category')?.setValue(category);
+    this.matchingCategories = [];
+  }
+
 }
