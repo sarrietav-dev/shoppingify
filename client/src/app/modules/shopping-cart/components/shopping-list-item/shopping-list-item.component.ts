3b94522bc1696e21372c92be2e1deea29efc30a4
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-08-03 11:00:00 -0500

    🚧 Male the increment, decrement and delete buttons functional

--- client/src/app/modules/shopping-cart/components/shopping-list-item/shopping-list-item.component.ts
+++ client/src/app/modules/shopping-cart/components/shopping-list-item/shopping-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list-item',
@@ -7,11 +7,32 @@ import { Component, Input } from '@angular/core';
 })
 export class ShoppingListItemComponent {
   @Input() name!: string;
-  @Input() amount!: number;
+  @Input() amount: number = 0;
+  @Input() id: string = '';
+
+  @Output('increment') incrementEvent = new EventEmitter<string>();
+  @Output('decrement') decrementEvent = new EventEmitter<string>();
+  @Output('delete') deleteEvent = new EventEmitter<string>();
 
   isEditing = false;
 
   toggleEdit() {
     this.isEditing = !this.isEditing;
+  }
+
+  increment() {
+    this.amount++;
+    this.incrementEvent.emit(this.id ?? 'id');
+  }
+
+  decrement() {
+    if (this.amount - 1 < 1) return;
+
+    this.amount--;
+    this.decrementEvent.emit(this.id ?? 'id');
+  }
+
+  delete() {
+    this.deleteEvent.emit(this.id ?? 'id');
   }
 }
