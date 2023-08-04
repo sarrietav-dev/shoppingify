2e736f927f0917c160663e1d48be76aeab230442
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-08-03 21:12:29 -0500

    🚧  Add getCart method

--- client/src/app/core/services/cart/shopping-cart.service.ts
+++ client/src/app/core/services/cart/shopping-cart.service.ts
@@ -1,8 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+type Cart = {
+  id: string;
+  name: string;
+  lineItems: LineItem[];
+  cartCount: number;
+  itemCount: number;
+  checkedItems: number;
+}
+
+type LineItem = {
+  quantity: number;
+  isChecked: boolean;
+  product: Product;
+}
+
+type Product = {
+  id: string;
+  name: string;
+  note: string;
+  category: string;
+  image: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShoppingCartService {
-  constructor() {}
+  private baseUrl = `${environment.baseUrl}/api/ShoppingCart`;
+
+  constructor(private http: HttpClient) {}
+
+  getCart(id: string) {
+    return this.http.get<Cart>(this.baseUrl + `/${id}`);
+  }
 }
