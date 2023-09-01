4935cbab7aa0f66755415dfacf9ee884a35e67d7
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-01 07:22:17 -0500

    Fix nonexistent methods on Products persistence

--- shoppingify/Products/Infrastructure/Persistence/ProductEntityConfig.cs
+++ shoppingify/Products/Infrastructure/Persistence/ProductEntityConfig.cs
@@ -8,23 +8,18 @@ public class ProductEntityConfiguration : IEntityTypeConfiguration<Product>
 {
     public void Configure(EntityTypeBuilder<Product> builder)
     {
-        builder.ToTable("Products");
-
         builder.HasKey(x => x.Id.Value);
         builder.HasAlternateKey(x => x.Owner.Value);
 
         builder.Property(x => x.Id)
-            .HasColumnName("Id")
             .HasConversion(x => x.Value, x => new ProductId(x))
             .IsRequired();
 
         builder.Property(x => x.Owner)
-            .HasColumnName("Owner")
             .HasConversion(x => x.Value, x => new ProductOwner(x))
             .IsRequired();
 
         builder.Property(x => x.Name)
-            .HasColumnName("Name")
             .IsRequired();
 
         builder.Property(x => x.Note);
