fdb3b66809f6428ef436dbc7a82e72db3b11db8a
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-01 18:26:16 -0500

    Create CartOwner aggregate and repository

---
+++ shoppingify/Cart/Domain/CartOwner.cs
@@ -0,0 +1,49 @@
+﻿namespace Shoppingify.Cart.Domain;
+
+public class CartOwner
+{
+    private CartId? _activeCartId;
+    public required CartOwnerId Id { get; init; }
+
+    public CartId? ActiveCartId
+    {
+        get => _activeCartId;
+        init => _activeCartId = value;
+    }
+
+    public Cart CreateCart(string name, IEnumerable<CartItem>? cartItems)
+    {
+        if (ActiveCartId is not null)
+            throw new InvalidOperationException("Cannot create a new cart while there is an active one");
+
+        var cart = new Cart
+        {
+            Id = new CartId(Guid.NewGuid()),
+            CartOwnerId = Id,
+            Name = name
+        };
+
+        _activeCartId = cart.Id;
+
+        if (cartItems is not null)
+            cart.UpdateList(cartItems);
+
+        return cart;
+    }
+    
+    public void CompleteCart()
+    {
+        if (ActiveCartId is null)
+            throw new InvalidOperationException("Cannot complete a cart while there is no active one");
+
+        _activeCartId = null;
+    }
+    
+    public void CancelCart()
+    {
+        if (ActiveCartId is null)
+            throw new InvalidOperationException("Cannot cancel a cart while there is no active one");
+
+        _activeCartId = null;
+    }
+}
---
+++ shoppingify/Cart/Domain/CartOwnerRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Shoppingify.Cart.Domain;
+
+public interface ICartOwnerRepository
+{
+    public CartOwner? Get(CartOwnerId id);
+}
