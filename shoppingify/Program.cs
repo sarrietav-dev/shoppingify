a79451746bd01710d2271df302bfc03894c75a6a
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-08-31 22:08:09 -0500

    Remove old backend

--- shoppingify/Program.cs
+++ shoppingify/Program.cs
@@ -1,32 +1,11 @@
-using Microsoft.EntityFrameworkCore;
-using shoppingify;
-using shoppingify.Repositories;
-using shoppingify.Repositories.Impl;
-using shoppingify.Services;
-using shoppingify.Services.Impl;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", builder =>
-    {
-        builder.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-    });
-});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ShoppingContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-//builder.Services.AddDbContext<ShoppingContext>(options => options.UseInMemoryDatabase("Shoppingify"));
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
 
 var app = builder.Build();
 
@@ -36,8 +15,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
