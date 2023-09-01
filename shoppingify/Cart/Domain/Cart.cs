eb33644485c9cb994870087daf9dd933aac5d56c
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-01 07:44:14 -0500

    Add setter to Cart.State

--- shoppingify/Cart/Domain/Cart.cs
+++ shoppingify/Cart/Domain/Cart.cs
@@ -2,13 +2,32 @@ namespace Shoppingify.Cart.Domain;
 
 public class Cart
 {
+    private CartState _state = CartState.Active;
     public required CartId Id { get; init; }
     public required CartOwnerId CartOwnerId { get; init; }
     public required string Name { get; init; }
-    public CartState State { get; private set; } = CartState.Active;
+
+    public CartState State
+    {
+        get => _state;
+        private set
+        {
+            _state = _state switch
+            {
+                CartState.Completed => throw new InvalidOperationException("Cannot change state of a completed cart"),
+                CartState.Canceled => throw new InvalidOperationException("Cannot change state of a canceled cart"),
+                _ => value
+            };
+        }
+    }
 
     public void Complete()
     {
         State = CartState.Completed;
+    }
+    
+    public void Cancel()
+    {
+        State = CartState.Canceled;
     }
 }
