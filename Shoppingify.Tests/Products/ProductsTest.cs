39ce18f116736ec7263e41be69196cd2475853fa
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-02 12:34:49 -0500

    Add Tests for product

---
+++ Shoppingify.Tests/Products/ProductsTest.cs
@@ -0,0 +1,46 @@
+﻿using shoppingify.Products.Domain;
+
+namespace Shoppingify.Tests.Products;
+
+public class ProductsTest
+{
+    [Fact]
+    public void TestCreateProduct()
+    {
+        var product = new Product
+        {
+            Id = new ProductId(Guid.NewGuid()),
+            Owner = new ProductOwner("y1281"),
+            Name = "name",
+            Category = "category",
+            Note = "note", Image = "image"
+        };
+        Assert.Equal("name", product.Name);
+        Assert.Equal("category", product.Category);
+        Assert.Equal("note", product.Note);
+        Assert.Equal("image", product.Image);
+    }
+
+    [Fact]
+    public void TestTwoProductsWithTheSameIdAreEqual()
+    {
+        var id = new ProductId(Guid.NewGuid());
+        var product1 = new Product
+        {
+            Id = id,
+            Owner = new ProductOwner("y1281"),
+            Name = "name",
+            Category = "category",
+            Note = "note", Image = "image"
+        };
+        var product2 = new Product
+        {
+            Id = id,
+            Owner = new ProductOwner("y1281"),
+            Name = "name",
+            Category = "category",
+            Note = "note", Image = "image"
+        };
+        Assert.Equal(product1, product2);
+    }
+}
