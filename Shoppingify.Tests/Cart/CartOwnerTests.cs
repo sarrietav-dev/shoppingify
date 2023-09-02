2df60953a3b2d0cb8f1f3d2a580b3ce6b1a3f478
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-02 12:53:17 -0500

    Add tests for cart owner

---
+++ Shoppingify.Tests/Cart/CartOwnerTests.cs
@@ -0,0 +1,62 @@
+﻿using shoppingify.Cart.Domain;
+
+namespace Shoppingify.Tests.Cart;
+
+public class CartOwnerTests
+{
+    [Fact]
+    public void Should_Create_CartOwner()
+    {
+        var cartOwner = new CartOwner
+        {
+            Id = new CartOwnerId("y1281")
+        };
+
+        Assert.Equal("y1281", cartOwner.Id.Value);
+        Assert.Null(cartOwner.ActiveCartId);
+    }
+    
+    [Fact]
+    public void Should_Create_Cart()
+    {
+        var cartOwner = new CartOwner
+        {
+            Id = new CartOwnerId("y1281")
+        };
+
+        var cart = cartOwner.CreateCart("My Cart", null);
+
+        Assert.Equal("My Cart", cart.Name);
+        Assert.Equal(CartState.Active, cart.State);
+        Assert.Equal(0, cart.CartItems.Count);
+        Assert.Equal(cart.Id, cartOwner.ActiveCartId);
+    }
+    
+    [Fact]
+    public void Should_Complete_Cart()
+    {
+        var cartOwner = new CartOwner
+        {
+            Id = new CartOwnerId("y1281")
+        };
+
+        var cart = cartOwner.CreateCart("My Cart", null);
+        cartOwner.CompleteCart();
+
+        Assert.Null(cartOwner.ActiveCartId);
+    }
+    
+    [Fact]
+    public void Should_Cancel_Cart()
+    {
+        var cartOwner = new CartOwner
+        {
+            Id = new CartOwnerId("y1281")
+        };
+
+        var cart = cartOwner.CreateCart("My Cart", null);
+        cartOwner.CancelCart();
+
+        Assert.Null(cartOwner.ActiveCartId);
+    }
+}
