cf6f051e22ca06af6f392d89d8a23171942a0ee5
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-01 19:33:05 -0500

    Add CartOwner EF Core Config

---
+++ shoppingify/Cart/Infrastructure/Persistence/CartOwnerEntityConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using shoppingify.Cart.Domain;
+
+namespace shoppingify.Cart.Infrastructure.Persistence;
+
+public class CartOwnerEntityConfig : IEntityTypeConfiguration<CartOwner>
+{
+    public void Configure(EntityTypeBuilder<CartOwner> builder)
+    {
+        builder.HasKey(c => c.Id.Value);
+        builder.Property(c => c.Id).ValueGeneratedNever()
+            .HasConversion(id => id.Value, value => new CartOwnerId(value));
+        builder.Property(c => c.ActiveCartId)
+            .HasConversion(id => id!.Value, value => new CartId(value));
+    }
+}
