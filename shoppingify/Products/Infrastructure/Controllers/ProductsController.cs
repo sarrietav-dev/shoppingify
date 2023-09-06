4ea42ecade64b4724be93f0d3175ce33c4d25fc9
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-06 15:05:56 -0500

    Use Authentication header and change endpoint path in ProductsController.cs

--- shoppingify/Products/Infrastructure/Controllers/ProductsController.cs
+++ shoppingify/Products/Infrastructure/Controllers/ProductsController.cs
@@ -1,17 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using shoppingify.IAM.Application;
 using shoppingify.Products.Application;
 
 namespace shoppingify.Products.Infrastructure.Controllers;
 
 [ApiController]
-[Route("api/v1/[controller]")]
+[Route("api/v1/me/products")]
 public class ProductsController : ControllerBase
 {
     private readonly ProductApplicationService _applicationService;
+    private readonly IAuthenticationProviderService _authenticationProviderService;
 
-    public ProductsController(ProductApplicationService applicationService)
+    public ProductsController(ProductApplicationService applicationService, IAuthenticationProviderService authenticationProviderService)
     {
         _applicationService = applicationService;
+        _authenticationProviderService = authenticationProviderService;
     }
 
     [HttpGet("{id:guid}")]
@@ -26,26 +29,37 @@ public class ProductsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetAll(string id)
+    public async Task<IActionResult> GetAll()
     {
+        var id = await GetAuthorizationToken();
+        
         var products = await _applicationService.GetAll(id);
 
         return Ok(products);
     }
 
     [HttpPost]
-    public async Task<IActionResult> Add(string id, AddProductCommand product)
+    public async Task<IActionResult> Add(AddProductCommand product)
     {
-        await _applicationService.Add(id, product);
+        var id = await GetAuthorizationToken();
+        
+        await _applicationService.Add(ownerId: id, product);
 
         return Ok();
     }
 
-    [HttpDelete("{id}")]
+    [HttpDelete("{id:guid}")]
     public async Task<IActionResult> Delete(Guid id)
     {
         await _applicationService.Delete(id);
 
         return Ok();
+    }
+    
+    private async Task<string> GetAuthorizationToken()
+    {
+        var uid = Request.Headers["Authorization"].ToString().Split(" ")[1];
+        var cartOwnerId = await _authenticationProviderService.VerifyToken(uid);
+        return cartOwnerId;
     }
 }
