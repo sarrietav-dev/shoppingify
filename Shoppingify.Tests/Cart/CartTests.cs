3033e39a98758a984d28045e65105e973a3fdd72
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-06 15:00:43 -0500

    Remove non public methods

--- Shoppingify.Tests/Cart/CartTests.cs
+++ Shoppingify.Tests/Cart/CartTests.cs
@@ -43,36 +43,6 @@ public class CartTests
     }
 
     [Fact]
-    public void Should_Complete()
-    {
-        var cart = new shoppingify.Cart.Domain.Cart
-        {
-            Id = new CartId(Guid.NewGuid()),
-            CartOwnerId = new CartOwnerId("y1281"),
-            Name = "My Cart"
-        };
-
-        cart.Complete();
-
-        Assert.Equal(CartState.Completed, cart.State);
-    }
-
-    [Fact]
-    public void Should_Cancel()
-    {
-        var cart = new shoppingify.Cart.Domain.Cart
-        {
-            Id = new CartId(Guid.NewGuid()),
-            CartOwnerId = new CartOwnerId("y1281"),
-            Name = "My Cart"
-        };
-
-        cart.Cancel();
-
-        Assert.Equal(CartState.Canceled, cart.State);
-    }
-
-    [Fact]
     public void Should_Update_List()
     {
         var cart = new shoppingify.Cart.Domain.Cart
@@ -95,49 +65,5 @@ public class CartTests
         cart.UpdateList(cartItems);
 
         Assert.Equal(cartItems, cart.CartItems);
-    }
-
-    [Fact]
-    public void Should_Not_Complete_Twice()
-    {
-        var cart = new shoppingify.Cart.Domain.Cart
-        {
-            Id = new CartId(Guid.NewGuid()),
-            CartOwnerId = new CartOwnerId("y1281"),
-            Name = "My Cart"
-        };
-
-        var exception = Assert.Throws<InvalidOperationException>(Act);
-
-        Assert.Equal("Cannot change state of a completed cart", exception.Message);
-        return;
-
-        void Act()
-        {
-            cart.Complete();
-            cart.Complete();
-        }
-    }
-    
-    [Fact]
-    public void Should_Not_Cancel_Twice()
-    {
-        var cart = new shoppingify.Cart.Domain.Cart
-        {
-            Id = new CartId(Guid.NewGuid()),
-            CartOwnerId = new CartOwnerId("y1281"),
-            Name = "My Cart"
-        };
-
-        var exception = Assert.Throws<InvalidOperationException>(Act);
-
-        Assert.Equal("Cannot change state of a canceled cart", exception.Message);
-        return;
-
-        void Act()
-        {
-            cart.Cancel();
-            cart.Cancel();
-        }
     }
 }
