6866bbd24aa45abcd3fcbe6c3bb617370f6fa5d1
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-12 21:24:21 -0500

    Add routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ItemListModule } from './modules/item-list/item-list.module';
-import { ShoppingCartModule } from './modules/shopping-cart/shopping-cart.module';
-import { HttpClientModule } from '@angular/common/http';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    CoreModule,
+    BrowserModule,
+    CommonModule,
     BrowserAnimationsModule,
-    ItemListModule,
-    ShoppingCartModule,
     HttpClientModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
