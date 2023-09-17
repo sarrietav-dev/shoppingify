e88c5237b878fdc2bc591fbe7f68b59b6441e757
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-17 14:58:00 -0500

    Create the auth service

---
+++ client/src/app/core/services/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Auth, GoogleAuthProvider, signInWithPopup, signInWithRedirect } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private fbAuth: Auth, private http: HttpClient) { }
+
+  async signInWithGoogle() {
+    const userCredentials = await signInWithPopup(this.fbAuth, new GoogleAuthProvider());
+  }
+
+  async signInWithGoogleRedirect() {
+    const userCredentials = await signInWithRedirect(this.fbAuth, new GoogleAuthProvider());
+  }
+
+  async signOut() {
+    await this.fbAuth.signOut();
+  }
+
+  get user() {
+    return this.fbAuth.currentUser;
+  }
+}
