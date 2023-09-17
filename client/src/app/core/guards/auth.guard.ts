3253ad13cbdde08c08aa2840ae3546d52158367f
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-17 14:12:49 -0500

    Create Guard

---
+++ client/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard {
+  constructor(private fbAuth: Auth) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  
+}
