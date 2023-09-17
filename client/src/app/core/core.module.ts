91a4800e89f989a7be6e323dc30376bce9547974
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-09-17 17:04:46 -0500

    Use sdk icons

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   exports: [NavbarComponent],
   declarations: [NavbarComponent],
-  imports: [CommonModule, MatTooltipModule],
+  imports: [CommonModule, MatTooltipModule, MatMenuModule, MatIconModule],
 })
 export class CoreModule {}
