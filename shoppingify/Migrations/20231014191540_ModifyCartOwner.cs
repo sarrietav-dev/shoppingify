aa570b7a23e140301e8eec5a282818b36c908c5b
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-10-14 14:47:51 -0500

    Add migration for CartOwner

---
+++ shoppingify/Migrations/20231014191540_ModifyCartOwner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shoppingify.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyCartOwner : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_CartOwners_CartOwnerId",
+                table: "Carts");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ActiveCart",
+                table: "CartOwners",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActiveCart",
+                table: "CartOwners");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_CartOwners_CartOwnerId",
+                table: "Carts",
+                column: "CartOwnerId",
+                principalTable: "CartOwners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
